fa5722046eb3800897b8271c2babbef55e9fa1c0
Lucas <tech@firebrandwebsolutions.com>
2019-10-06 00:05:38 -0700

    Include Cart container component in exports.

--- src/flux/index.js
+++ src/flux/index.js
@@ -1,8 +1,10 @@
+import { Cart } from './containers'
 import { CartActions, ActionTypes as CartActionTypes } from './actions'
 import { CartStore } from './store'
 import { CartContext, createCartContextManager } from './context'
 
 export {
+  Cart,
   CartActions as Actions,
   CartActionTypes as ActionTypes,
   CartContext as Context,
